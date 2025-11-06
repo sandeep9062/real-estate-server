22f815cb81595b59d195982b276f0b7f4b3e7ea8
Sandeep Saini <sandeepsaini157219@gmail.com>
2025-11-07 01:56:29 +0530

    upda24242

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://real-estate-client-cyan.vercel.app/"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://real-estate-client-cyan.vercel.app"],
     credentials: true,
   })
 );
