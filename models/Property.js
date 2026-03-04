97ff5838e7e5cab5f563704f742a4c93de92c615
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-03-04 14:05:41 +0530

    facing-12

--- models/Property.js
+++ models/Property.js
@@ -91,7 +91,16 @@ const propertySchema = new mongoose.Schema(
     },
     facing: {
       type: String,
-      enum: ["North", "South", "East", "West", "North-East", "North-West"],
+      enum: [
+        "North",
+        "South",
+        "East",
+        "West",
+        "North-East",
+        "North-West",
+        "South-East",
+        "South-West",
+      ],
     },
 
     price: { type: Number, min: 0, required: true, index: true },
