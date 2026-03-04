1b1e8dabecd30d99ad744749a2ccdbb11a0ff342
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-03-04 14:45:22 +0530

    facing-1223

--- models/Property.js
+++ models/Property.js
@@ -55,6 +55,7 @@ const propertySchema = new mongoose.Schema(
         "Apartment/Flat",
         "House/Villa",
         "Land/Plot",
+        "Builder Floor",
         "PG/Hostel",
         "Farmhouse",
         "Retail",
