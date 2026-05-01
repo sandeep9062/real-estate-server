31bb387a2a8d4595e9df7a35b708a017d117d32d
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-05-01 21:39:09 +0530

    cursor-emials

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,14 +1,17 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // use SSL
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT), // Port 587 as a number
+  secure: false, // TLS ke liye false (Port 465 ke liye true hota hai)
   auth: {
-    user: process.env.OWNER_EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
+  // Render connectivity fixes
+  connectionTimeout: 10000,
   tls: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+    minVersion: "TLSv1.2",
+  },
 });
